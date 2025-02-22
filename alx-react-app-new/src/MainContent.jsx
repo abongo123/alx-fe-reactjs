ac84441cdb09370f2706bc61f9f2086d2fa28097
abongo123 <103944706+abongo123@users.noreply.github.com>
2025-02-22 14:54:56 -0800

    Add files via upload

---
+++ alx-react-app-new/src/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent() {
+    return (
+      <main style={{backgroundColor:'lightgreen', textAlign: 'center', }}>
+        <p style={{textAlign:'center', color:'black', fontFamily:'serif', fontWeight:'normal'}}>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    )
+  }
+  export default MainContent;
