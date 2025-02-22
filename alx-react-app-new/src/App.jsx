86a8246b75d8393a150ecd0d2403b4a46e47e758
Stephen Abongo <stephenabongo48@gmail.com>
2025-02-22 15:09:17 -0800

    Created counter.jsx file and modified pp.jsx file

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 import UserProfile from './UserProfile';
+import Counter from "./Counter";
 
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
