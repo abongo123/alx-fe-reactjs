3fce03b11da14121e4d2ee35a121c432bda190bc
Stephen Abongo <stephenabongo48@gmail.com>
2025-02-22 17:49:44 -0800

    Added files and modified app.jsx file

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,49 +1,14 @@
-import WelcomeMessage from "./WelcomeMessage";
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
-import UserProfile from './UserProfile';
 
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import ProfilePage from './UserProfile';
+import WelcomeMessage from "./WelcomeMessage";
+import Header from "./Header";
+import Footer from "./Footer"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return (
-    <>
-      <Header /> {/* Added Header component */}
-      <MainContent /> {/* Added MainContent component */}
-      <Footer /> {/* Added Footer component */}
-
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-
-
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <WelcomeMessage />
-    </>
-  );
+  return <ProfilePage userData={userData} />;
 }
 
-export default App
+export default App;
