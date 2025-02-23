e7f4e86f94f4344447194e3636336daa4ede380c
Stephen Abongo <stephenabongo48@gmail.com>
2025-02-22 18:24:24 -0800

    Replaced props with useContext

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,18 @@
-const UserProfile = (props) => {
-    return (
-  <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-   <h2 style={{ color: 'blue' }}>{props.name}</h2>
-   <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-   <p>Bio: {props.bio}</p>
- </div>
-    );
-  };
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
 
-  export default UserProfile;
+const UserProfile = () => {
+  const user = useContext(UserContext);
+
+  return (
+    <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
+      <h2 style={{ color: "blue" }}>{user.name}</h2>
+      <p>
+        Age: <span style={{ fontWeight: "bold" }}>{user.age}</span>
+      </p>
+      <p>Bio: {user.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
