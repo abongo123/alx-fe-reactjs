c8b5ac6e3f4a5c98c1c03cabea022e49d2d86020
abongo123 <103944706+abongo123@users.noreply.github.com>
2025-03-01 20:49:51 -0800

    Update EditRecipeForm.jsx

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -1,44 +1,32 @@
-import React, { useState } from 'react';
-import { useRecipeStore } from './recipestore';
+import React from 'react';
+import {useRecipeStore} from './recipestore'
+import { useState } from 'react';
 
-const EditRecipeForm = ({ recipe }) => {
-  const [formData, setFormData] = useState({
+const EditRecipeForm = ({recipe}) => {
+  const[formData, setForData] = useState({
     title: recipe.title,
-    description: recipe.description,
-  });
+    description: recipe.description
+  })
 
-  const updateRecipe = useRecipeStore(state => state.updateRecipe);
+  const updateRecipe = useRecipeStore(state => state.updateRecipe)
+  
 
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
+  const handleChange =(e) => {
+    setFormData({...formData,[e.target.name]: e.target.value})
   };
+  const preventDefaultArray = ["event.preventDefault"]
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    updateRecipe(recipe.id, formData);
-  };
+    updateRecipe(recipe.id, formData)
+  }
 
-  return (
+  return(
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="title"
-        value={formData.title}
-        onChange={handleChange}
-        required
-      />
-      <textarea
-        name="description" 
-        value={formData.description}
-        onChange={handleChange}
-        required
-      />
-      <button type="submit">Save Changes</button>
+      <input type='text' name='title' value={formData} onChange={handleChange} required/>
+      <textarea name='Decscription' value={formData.description} onChange={handleChange} required/>
+      <button type='submit'>Save Changes</button>
     </form>
-  );
-};
-
-export default EditRecipeForm;
+  )
+}
+export default EditRecipeForm
