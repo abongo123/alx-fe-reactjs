ee973ef9740e69bbecfd177bf5e4d9b6421e9107
abongo123 <103944706+abongo123@users.noreply.github.com>
2025-03-01 20:45:33 -0800

    Update EditRecipeForm.jsx

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -1,31 +1,44 @@
-import React from 'react';
-import {useRecipeStore} from './recipestore'
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { useRecipeStore } from './recipestore';
 
-const EditRecipeForm = ({recipe}) => {
-  const[formData, setForData] = useState({
+const EditRecipeForm = ({ recipe }) => {
+  const [formData, setFormData] = useState({
     title: recipe.title,
-    description: recipe.description
-  })
+    description: recipe.description,
+  });
 
-  const updateRecipe = useRecipeStore(state => state.updateRecipe)
+  const updateRecipe = useRecipeStore(state => state.updateRecipe);
 
-  const handleChange =(e) => {
-    setFormData({...formData,[e.target.name]: e.target.value})
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    updateRecipe(recipe.id, formData)
-  }
+    updateRecipe(recipe.id, formData);
+  };
 
-  return(
+  return (
     <form onSubmit={handleSubmit}>
-      <input type='text' name='title' value={formData} onChange={handleChange} required/>
-      <textarea name='Decscription' value={formData.description} onChange={handleChange} required/>
-      <button type='submit'>Save Changes</button>
+      <input
+        type="text"
+        name="title"
+        value={formData.title}
+        onChange={handleChange}
+        required
+      />
+      <textarea
+        name="description" 
+        value={formData.description}
+        onChange={handleChange}
+        required
+      />
+      <button type="submit">Save Changes</button>
     </form>
-  )
-}
+  );
+};
 
-export default EditRecipeForm
+export default EditRecipeForm;
