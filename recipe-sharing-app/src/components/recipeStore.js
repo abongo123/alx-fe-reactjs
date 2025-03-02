2d67eecd7fe2c1cf2646dddcfe716c051527523b
abongo123 <103944706+abongo123@users.noreply.github.com>
2025-03-01 20:35:35 -0800

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,1 +1,28 @@
+import create from 'zustand'
+
+const useRecipeStore = create(set => ({
+  recipes: [],
+
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes }),
+
+  deleteRecipeL: (id) =>
+    set ((state) => ({
+        recipes: state.recipes.filter((recipe) => recipe.id !==id)
+    })),
+
+    updateRecipe: (id,updateRecipe) =>
+        set((state) => ({recipes: state.recipes.map((recipe) => 
+        recipe.id ==id ? {...recipe, ...updateRecipe} : recipe),
+    })),
+    recommendations: [],
+    generateRecommendation: () => set(state => {
+        const recommended = state.recipes.filter(recipe =>
+            state.favourites.includes (recipe.id) && Math.random()
+            >0.5
+        )
+    })
+}));
+
+export default useRecipeStore;
 
