f849f408cde945c6e92dcc01215864d8697ba7af
Stephen Abongo <stephenabongo48@gmail.com>
2025-03-09 04:58:54 -0700

    installed formik and created formik.js file

---
+++ form-handling-react/src/components/formikForm.js
@@ -0,0 +1,45 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+  username: Yup.string().required("Username is required"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
+});
+
+function FormikForm() {
+  return (
+    <Formik
+      initialValues={{ username: "", email: "", password: "" }}
+      validationSchema={validationSchema}
+      onSubmit={(values, { resetForm }) => {
+        console.log("Form submitted:", values);
+        alert("Registration successful!");
+        resetForm(); // Clear form after submission
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <div>
+            <Field type="text" name="username" placeholder="Your Username" />
+            <ErrorMessage name="username" component="div" style={{ color: "red" }} />
+          </div>
+
+          <div>
+            <Field type="email" name="email" placeholder="Input Email" />
+            <ErrorMessage name="email" component="div" style={{ color: "red" }} />
+          </div>
+
+          <div>
+            <Field type="password" name="password" placeholder="Your Password" />
+            <ErrorMessage name="password" component="div" style={{ color: "red" }} />
+          </div>
+
+          <button type="submit" disabled={isSubmitting}>Register</button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export default FormikForm;
