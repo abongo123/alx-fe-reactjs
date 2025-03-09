2cff2f58375f5624e10d05099840c6785065192b
Stephen Abongo <stephenabongo48@gmail.com>
2025-03-09 06:31:20 -0700

    updated RegistrationForm file

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,62 +1,51 @@
-import { Formik, Form, Field } from "formik";
 import { useState } from "react";
 
 function RegistrationForm() {
-  const [errors, setErrors] = useState({});
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let newErrors = {};
+
+    if (!formData.username) newErrors.username = "Username is required";
+    if (!formData.email) newErrors.email = "Email is required";
+    if (!formData.password) newErrors.password = "Password is required";
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+      return;
+    }
+
+    console.log("Form submitted:", formData);
+    alert("Registration successful!");
+    setFormData({ username: "", email: "", password: "" });
+    setErrors({});
+  };
+
 
   return (
-    <Formik
-      initialValues={{ username: "", email: "", password: "" }}
-      onSubmit={(values, { resetForm }) => {
-        let newErrors = {};
+    <form onSubmit={handleSubmit}>
+        <input type='text' name='username' placeholder='Your Username' value={username} onChange={handleChange}/>
+        <input type='email' name='email' placeholder='Input Email' value={email} onChange={handleChange}/>
+        <input type='password' name='password' placeholder='Your Password' value={password} onChange={handleChange}/>
+        <button type='Submit'>Register</button>
 
-        if (!values.username) {
-          newErrors.username = "Username is required";
-        }
-        if (!values.email) {
-          newErrors.email = "Email is required";
-        } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-          newErrors.email = "Invalid email format";
-        }
-        if (!values.password) {
-          newErrors.password = "Password is required";
-        } else if (values.password.length < 6) {
-          newErrors.password = "Password must be at least 6 characters";
-        }
 
-        if (Object.keys(newErrors).length > 0) {
-          setErrors(newErrors);
-          return;
-        }
 
-        console.log("Form submitted:", values);
-        alert("Registration successful!");
-        resetForm();
-        setErrors({});
-      }}
-    >
-      {({ values, handleChange, handleSubmit }) => (
-        <Form onSubmit={handleSubmit}>
-          <div>
-            <Field type="text" name="username" placeholder="Your Username" onChange={handleChange} />
-            {errors.username && <div style={{ color: "red" }}>{errors.username}</div>}
-          </div>
 
-          <div>
-            <Field type="email" name="email" placeholder="Input Email" onChange={handleChange} />
-            {errors.email && <div style={{ color: "red" }}>{errors.email}</div>}
-          </div>
-
-          <div>
-            <Field type="password" name="password" placeholder="Your Password" onChange={handleChange} />
-            {errors.password && <div style={{ color: "red" }}>{errors.password}</div>}
-          </div>
-
-          <button type="submit">Register</button>
-        </Form>
-      )}
-    </Formik>
-  );
+    </form>
+  )
 }
 
-export default RegistrationForm;
+export default RegistrationForm
