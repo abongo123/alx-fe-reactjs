4dac15e2b9b7b8d1a29355fc84a910796bbe6a4e
abongo123 <103944706+abongo123@users.noreply.github.com>
2025-03-10 00:38:29 -0700

    Update ProtectedRoute.jsx

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,6 +1,9 @@
 import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext"; // Import useAuth
 
-function ProtectedRoute({ element, isAuthenticated }) {
+function ProtectedRoute({ element }) {
+  const { isAuthenticated } = useAuth(); // Get auth state
+
   return isAuthenticated ? element : <Navigate to="/login" replace />;
 }
 
