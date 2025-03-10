cce34dd63ee1b71954c2187d947a3ab0a51dd35f
Stephen Abongo <stephenabongo48@gmail.com>
2025-03-10 00:00:09 -0700

    Added profile component

--- react-router-advanced/src/Components/Profile.jsx
+++ react-router-advanced/src/Components/Profile.jsx
@@ -1,16 +1,18 @@
-import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
-const Profile = () => {
+function Profile() {
   return (
     <div>
-      <h1>Profile Page</h1>
+      <h2>Profile</h2>
       <nav>
-        <Link to="details">Profile Details</Link> | <Link to="settings">Profile Settings</Link>
+        <ul>
+          <li><Link to="details">Profile Details</Link></li>
+          <li><Link to="settings">Profile Settings</Link></li>
+        </ul>
       </nav>
       <Outlet />
     </div>
   );
-};
+}
 
 export default Profile;
