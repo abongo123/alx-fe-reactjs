bd47f9e8b76e331321e186cd5193ac763b3b38d1
abongo123 <103944706+abongo123@users.noreply.github.com>
2025-03-10 00:12:34 -0700

    Update Profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,1 +1,18 @@
+mport { Outlet, Link } from "react-router-dom";
 
+function Profile() {
+  return (
+    <div>
+      <h2>Profile</h2>
+      <nav>
+        <ul>
+          <li><Link to="details">Profile Details</Link></li>
+          <li><Link to="settings">Profile Settings</Link></li>
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
+
+export default Profile;
